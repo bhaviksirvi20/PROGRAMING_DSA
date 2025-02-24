231fd07447d8028f8d67a0d92f155e12bbfbdd60
bhaviksirvi20 <bhaviksirvi964@gmail.com>
2025-02-24 19:57:31 +0530

    Updated Basic question answer

---
+++ If-Else  Nested If-Else  Ladder {Else-If}  Ternary operators/Ladder {Else-If}.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p, q;
+    cout << "Enter the first value: ";
+    cin >> p;
+    cout << "Enter the second value: ";
+    cin >> q;
+
+    if (p > q) {
+        cout << "Maximum value is: " << p << endl;
+    } 
+    else if (p < q) {
+        cout << "Maximum value is: " << q << endl;
+    } 
+    else {
+        cout << "value is equal." << endl;
+    }
+}

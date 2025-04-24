8cc4dbd6cb6d6d487f4c69fae330aeb415270312
bhaviksirvi20 <bhaviksirvi964@gmail.com>
2025-04-24 11:32:08 +0530

    Your commit message

---
+++ DSA Consept/Queue/.dist/1.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+using namespace std;
+
+void Enqueue(int queue[], int &size, int &rear, int user, int usersize) {
+    if (rear == usersize - 1) {
+        cout << "Queue is Full! Cannot insert more elements." << endl;
+    }
+    rear++;
+    for (int i = size; i <= rear; i++) {
+        queue[rear] = user;
+        cout << queue[i] << " " << endl;
+    }
+}
+
+
+void Dequeue(int queue[], int &size, int &rear, int user) {
+    size++;
+    for (int i = size; i <= rear; i++) {
+        cout << queue[i] << " " << endl;
+    }
+}
+
+void front(int queue[], int &size) {
+    cout << "Front value : " << queue[size] << endl;
+}
+
+void Rear(int queue[], int &rear) {
+    cout << "Last value : " << queue[rear] << endl;
+}
+
+void isEmpty(int &size, int &rear) {
+    if (size > rear) {
+        cout << "Queue is Empty ( blank )" << endl;
+    } else {
+        cout << "Queue is Not Empty ( Not blank )" << endl;
+    }
+}
+
+
+void isFull(int rear, int usersize) {
+    if (rear == usersize - 1) {
+        cout << "Queue is Full" << endl;
+    } else {
+        cout << "Queue is Not Full" << endl;
+    }
+}
+
+void Size(int &rear) {
+    cout << "Total value : " << rear + 1 << endl;
+}
+
+int main() {
+    int user, usersize;
+    int size = -1;
+    int rear = -1;
+
+    cout << "Enter The Array size: ";
+    cin >> usersize;
+
+    int* queue = new int[usersize];
+
+    int choice;
+    do {
+        cout << "1. Enqueue " << endl;
+        cout << "2. Dequeue " << endl;
+        cout << "3. Front " << endl;
+        cout << "4. Rear " << endl;
+        cout << "5. is Empty " << endl;
+        cout << "6. is Full " << endl;
+        cout << "7. Size " << endl;
+        cout << "8. Exit " << endl;
+        cout << "--------------------------------------" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter value to Enqueue: ";
+                cin >> user;
+                size = 0;
+                Enqueue(queue, size, rear, user , usersize);
+                break;
+
+            case 2:
+                Dequeue(queue, size, rear, user);
+                break;
+
+            case 3:
+                front(queue, size);
+                break;
+
+            case 4:
+                Rear(queue, rear);
+                break;
+
+            case 5:
+                isEmpty(size, rear);
+                break;
+
+            case 6:
+                isFull(rear, usersize);
+                break;
+
+            case 7: 
+                Size(rear);
+                break;
+
+            case 8:
+                cout << "Exit..." << endl;
+                break;
+
+            default:
+                cout << "Please Enter a number (1 to 8)" << endl;
+                break;
+        }
+        cout << "--------------------------------------" << endl;
+    } while (choice != 8);
+}
